144f54643bd415c82dd0308ca2555fab8d6fda9a
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-10 02:45:21 +0530

    Update main.tf
    Added AWS Caller Identity

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+data "aws_caller_identity" "current" {}
+
 locals {
   cluster_name = var.cluster_name
   instance_type = var.instance_type
