472c12f6d4da67da39259adeef860044985fb8de
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-10 00:56:24 +0530

    Update outputs.tf
    Added the following: 
    
    **Breakdown:**
    
    1.	**Public NATS IPs**: The IPs of NATS instances are being fetched from AWS instances and outputted.
    
    2.	**k8s version**: The version of the Kubernetes cluster is being outputted from an EKS cluster.
    
    3.	**CIDR Range**: The CIDR block of the main VPC is being outputted.
    
    4.	**AWS Account ID**: The AWS Account ID is retrieved using the aws_caller_identity data source.
    
    1. **VPC Name**: The VPC name is fetched from the tags associated with the aws_vpc.main resource. The "Name" tag is commonly used to identify the VPC.
    2. **Private NATS IPs**: This outputs the private IP addresses of the NATS instances using aws_instance.nats[*].private_ip.
    3. **Subnet IDs**: This outputs the IDs of the subnets using aws_subnet.subnets[*].id. Ensure that aws_subnet.subnets refers to the correct list of subnets in your Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -45,3 +45,38 @@ output "desired_nodes" {
   description = "Desired number of nodes"
   value       = local.desired_size
 }
+
+output "public_nats_ips" {
+  description = "Public NATS IPs"
+  value       = aws_instance.nats[*].public_ip
+}
+
+output "private_nats_ips" {
+  description = "Private NATS IPs"
+  value       = aws_instance.nats[*].private_ip
+}
+
+output "k8s_version" {
+  description = "Kubernetes version"
+  value       = aws_eks_cluster.eks_cluster.version
+}
+
+output "cidr_range" {
+  description = "CIDR Range"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "aws_account_id" {
+  description = "AWS Account ID"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "vpc_name" {
+  description = "VPC Name"
+  value       = aws_vpc.main.tags["Name"]
+}
+
+output "subnet_ids" {
+  description = "Subnet IDs"
+  value       = aws_subnet.subnets[*].id
+}
