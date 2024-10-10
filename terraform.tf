4e7ea09cdcca510b8a138457585e27c339076276
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-11 02:39:04 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -13,22 +13,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.47.0"
+      version = "~> 5.47.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4.3"
+      version = "~> 3.6.1"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.5"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+      version = "~> 2.3.4"
     }
   }
 
