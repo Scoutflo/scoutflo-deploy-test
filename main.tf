bbec6822d20b2130e21134cbd2ee7a4f8f79d093
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-11 16:43:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.28.0-eksbuild.1"
+  addon_version            = "v1.27.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
@@ -135,7 +135,7 @@ resource "aws_eks_addon" "kube-proxy" {
 resource "aws_eks_addon" "vpc-cni" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "vpc-cni"
-  addon_version            = "v1.16.1-eksbuild.1"
+  addon_version            = "v1.16.0-eksbuild.1"
   tags = {
     "eks_addon" = "vpc-cni"
     "terraform" = "true"
@@ -144,7 +144,7 @@ resource "aws_eks_addon" "vpc-cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "coredns"
-  addon_version            = "v1.11.1-eksbuild.4"
+  addon_version            = "v1.10.1-eksbuild.4"
   tags = {
     "eks_addon" = "coredns"
     "terraform" = "true"
