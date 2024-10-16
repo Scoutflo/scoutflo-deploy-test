0446d5dcf209f890712d02a7020b3a9ac411360f
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-16 23:57:53 +0530

    Update terraform.tf
    added helm

--- terraform.tf
+++ terraform.tf
@@ -34,6 +34,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.16.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   required_version = "~> 1.3"
