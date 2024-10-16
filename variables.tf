ff3d41238f93cbfb37440ba414743d2ff3449aff
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-16 19:30:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 variable "cluster_name" {
   description = "Cluster name"
   type = string
-  default = "deploy-eks-test-2"
+  default = "deploy-eks-test-3"
 }
 
 variable "instance_type" {
