5e8d33dd400c04eb969774ea4ce1720a84693803
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2024-10-17 01:37:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,18 @@ provider "aws" {
   region = var.region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+  }
+}
+
+
 # Filter out local zones, which are not currently supported 
 # with managed node groups
 data "aws_availability_zones" "available" {
@@ -76,7 +88,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "node-group-1-local.cluster_name"
 
       instance_types = [local.instance_type]
 
